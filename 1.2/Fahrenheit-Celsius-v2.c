3097f2ce3b6267a545d86f8a5ecf24b4e0e390ed
Dr_McFish <61105022+Dr-McFish@users.noreply.github.com>
2021-01-23 13:44:10 +0100

    Fix error in Fahrenheit-Celsius-v2.c

--- 1.2/Fahrenheit-Celsius-v2.c
+++ 1.2/Fahrenheit-Celsius-v2.c
@@ -16,7 +16,7 @@ main()
 	fahr = lower;
 	while(fahr <= upper) {
 		celsius = (5.0/9.0) * (fahr-32);
-		printf("%3f%6.1f\n", fahr, celsius);
+		printf("%3.0f%6.1f\n", fahr, celsius);
 		fahr = fahr + step;
 	}
 }
