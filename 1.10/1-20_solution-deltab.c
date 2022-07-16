c8fc683009de6a521bd632b536de651e25f07f20
Dr_McFish <oandroso@student.42.fr>
2022-07-16 15:19:40 +0200

    correct spmelling

--- 1.10/1-20_solution-deltab.c
+++ 1.10/1-20_solution-deltab.c
@@ -1,31 +1,33 @@
 /* 1-20_solution_deltab.c */
-/* Write a program deltab that replaces  tabs in the input with the proper number  of blanks to space to the next tab stop. Assume  a fixed ser of tab stops, say every n columns. Should n be a variable or a symbolic parameter. */
+/* Write a program deltab that replaces  tabs in the input with the proper
+ numberof blanks to space to the next tab stop. Assume  a fixed ser of tab
+ stops, say every n columns. Should n be a variable or a symbolic parameter. */
 
 #include <stdio.h>
 
 #define TAB_STOP 4
 
 int main() {
-	int current_space;
+	int current_column;
 	char c;
 
-	current_space = 0;
+	current_column = 0;
 	while ((c = getchar()) != EOF) {
 		switch(c) {
 			case '\t':
 				/* handle it. insert proper number of spaces */
 				putchar(' ');
-				for(current_space++; (current_space % TAB_STOP ) != 0; current_space++) {
+				for(current_column++; (current_column % TAB_STOP ) != 0; current_column++) {
 					putchar(' ');
 				}
 				break;
 			case '\n':
 				putchar(c);
-				current_space = 0;
+				current_column = 0;
 				break;
 			default:
 				putchar(c);
-				current_space++;
+				current_column++;
 				break;
 		}
 	}
